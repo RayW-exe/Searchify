da615172c7f9e77af5f58a192b085bd0a3779f02
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-04-20 23:54:55 +0300

    Jobs  updated

--- jobs.js
+++ jobs.js
@@ -1,19 +1,19 @@
 function sendDataForJob1() {
-    const jobStoredContent = document.querySelector('.job:essaysector-1').innerHTML;
+    const jobStoredContent = document.querySelector('.job-essaysector-1').innerHTML;
     localStorage.setItem('jobData', jobStoredContent);
     localStorage.setItem('jobType', '1');
     window.location.href = 'dashboard.html';
 }
 
 function sendDataForJob2() {
-    const jobStoredContent = document.querySelector('.job:designsector-2').innerHTML;
+    const jobStoredContent = document.querySelector('.job-designsector-2').innerHTML;
     localStorage.setItem('jobData', jobStoredContent);
     localStorage.setItem('jobType', '2');
     window.location.href = 'dashboard.html';
 }
 
 function sendDataForJob3() {
-    const jobStoredContent = document.querySelector('.job:developmentsector-3').innerHTML;
+    const jobStoredContent = document.querySelector('.job-developmentsector-3').innerHTML;
     localStorage.setItem('jobData', jobStoredContent);
     localStorage.setItem('jobType', '3');
     window.location.href = 'dashboard.html';
