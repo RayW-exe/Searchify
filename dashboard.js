6c59f7123ad82cc8e59be1e33433c09ed9fb5c82
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-04-20 23:34:27 +0300

    Dashboard updated

--- dashboard.js
+++ dashboard.js
@@ -1,19 +1,22 @@
-const jobData = localStorage.getItem('jobData');
-const jobType = localStorage.getItem('jobType');
-if (jobData && jobType) {
-    if (jobType === '1') {
+const jobContent = localStorage.getItem('jobData');
+const jobNumber = localStorage.getItem('jobType');
+if (jobContent && jobNumber) {
+    if (jobNumber === '1') {
         addJobItem1();
-    } else if (jobType === '2') {
+    } else if (jobNumber === '2') {
         addJobItem2();
-    } else if (jobType === '3') {
+    } else if (jobNumber === '3') {
         addJobItem3();
     }
     localStorage.removeItem('jobData');
     localStorage.removeItem('jobType');
 }
 
+// the guidelines i used
+// data is received and stored in variables. Then, passed to the new div and shown on screen. contact info becomes shown(summary)
+
 function addJobItem1() {
-    const jobItem = localStorage.getItem('jobData');
+    const jobItem = localStorage.getItem('jobContent');
     const newDiv = document.createElement('div');
     newDiv.classList.add('job-item1');
     newDiv.innerHTML = jobItem;
@@ -29,8 +32,9 @@ function addJobItem1() {
     }
 }
 
+
 function addJobItem2() {
-    const jobItem = localStorage.getItem('jobData');
+    const jobItem = localStorage.getItem('jobContent');
     const newDiv = document.createElement('div');
     newDiv.classList.add('job-item2');
     newDiv.innerHTML = jobItem;
@@ -46,8 +50,9 @@ function addJobItem2() {
     }
 }
 
+
 function addJobItem3() {
-    const jobItem = localStorage.getItem('jobData');
+    const jobItem = localStorage.getItem('jobContent');
     const newDiv = document.createElement('div');
     newDiv.classList.add('job-item3');
     newDiv.innerHTML = jobItem;
